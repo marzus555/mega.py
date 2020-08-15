d4aa3e09d6b2eed0852fb61d1ab36f6d61b0639d
marzus555 <62560070+marzus555@users.noreply.github.com>
2020-08-15 12:42:19 +0300

    Update mega.py

--- src/mega/mega.py
+++ src/mega/mega.py
@@ -724,7 +724,9 @@ class Mega:
                     encryptor.encrypt(block)
 
                 # fix for files under 16 bytes failing
-                if file_size <= 16:
+                if file_size > 16:
+                    i = 16
+                else:
                     i = 0
 
                 block = chunk[i:i + 16]
